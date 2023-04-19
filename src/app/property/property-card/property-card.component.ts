cbbc87db10c58abd99e66de44d7d472bd1f91314
16fo-1a0-520 <pavanasai.balla@gmail.com>
2023-04-19 19:58:45 +0530

    #Passing data between components using Input decorator

--- src/app/property/property-card/property-card.component.ts
+++ src/app/property/property-card/property-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-property-card',
@@ -8,14 +8,10 @@ import { Component, OnInit } from '@angular/core';
 export class PropertyCardComponent implements OnInit {
 
   constructor() { }
-  Property:any={
-    "Id":1,
-    "Name":"Birla House",
-    "Type":"Villa",
-    "Price":120000
-  }
 
-
+  @Input() inputProperty:any//throgh this we can receive list of properties
+  //passing data using Input directive
+  
   ngOnInit(): void {
   }
 
--- src/app/property/property-list/property-list.component.ts
+++ src/app/property/property-list/property-list.component.ts
@@ -9,6 +9,46 @@ export class PropertyListComponent implements OnInit {
 
   constructor() { }
 
+  properties:Array<any>=[
+    {
+      "Id":1,
+      "Name":"Tata House",
+      "Type":"Villa",
+      "Price":120000
+    },
+    {
+      "Id":2,
+      "Name":"Birla House",
+      "Type":"Flat",
+      "Price":150000
+    },
+    {
+      "Id":3,
+      "Name":"Ambani House",
+      "Type":"Villa",
+      "Price":160000
+    },
+    {
+      "Id":4,
+      "Name":"Adani House",
+      "Type":"House",
+      "Price":120000
+    },
+    {
+      "Id":5,
+      "Name":"Prabhas House",
+      "Type":"Villa",
+      "Price":150000
+    },
+    {
+      "Id":6,
+      "Name":"Moni House",
+      "Type":"Flat",
+      "Price":160000
+    }
+  
+  ]
+
   ngOnInit(): void {
   }
 
